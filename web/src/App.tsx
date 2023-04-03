fcb80a6344129982cfc353a1d03f6b8d502fb154
Bülent Özden <bulentozden2007@gmail.com>
2023-04-03 21:04:33 +0300

    Simplify and clean-up

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,8 +3,6 @@ import { Suspense, useEffect } from "react";
 import {
   createBrowserRouter,
   RouterProvider,
-  LoaderFunction,
-  useParams,
   Navigate,
 } from "react-router-dom";
 // i10n
@@ -25,33 +23,20 @@ import { ExaminePage } from "./components/pages/examine";
 import "./App.css";
 import {
   ANALYZER_DATA_URL,
-  CONFIG_TYPE,
   CV_DATA_URL,
   ILoaderData,
 } from "./helpers/appHelper";
 import axios from "axios";
-import {
-  DATASET_INFO_ROW_TYPE,
-  REPORTED_STATS_ROW_TYPE,
-  SUPPORT_MATRIX_ROW_TYPE,
-  TEXT_CORPUS_STATS_ROW_TYPE,
-} from "./helpers/tableHelper";
-import { CV_LANGUAGE_ROW } from "./helpers/cvHelper";
 
 function App() {
   // Store
   const { initDone, setInitDone } = useStore();
   const { setLangCode } = useStore();
-  let { lc, ver } = useParams();
 
   //
   // Loaders
   //
-  interface IResponse {
-    data: any[];
-    schema: any;
-  }
-  const doAxios = async (url: string) => {
+  const genericLoader = async (url: string) => {
     return await axios
       .get(url, { headers: { "Content-Type": "application/json" } })
       .then((response) => {
@@ -64,36 +49,6 @@ function App() {
       });
   };
 
-  // cvLanguages
-  const loadCvLanguages = async () => {
-    const url = `${CV_DATA_URL}/$cv_languages.json`;
-    return await doAxios(url);
-  };
-
-  // analyzerConfig
-  const loadConfig = async () => {
-    const url = `${ANALYZER_DATA_URL}/$config.json`;
-    return await doAxios(url);
-  };
-
-  // supportMatrix & matrixLoaded
-  const loadMatrix = async () => {
-    const url = `${ANALYZER_DATA_URL}/$support_matrix.json`;
-    return await doAxios(url);
-  };
-
-  // textCorpusStats
-  const loadTextCorpusStats = async () => {
-    const url = `${ANALYZER_DATA_URL}/$text_corpus_stats.json`;
-    return await doAxios(url);
-  };
-
-  // textCorpusStats
-  const loadReportedSentencesStats = async () => {
-    const url = `${ANALYZER_DATA_URL}/$reported.json`;
-    return await doAxios(url);
-  };
-
   // Loader common to all pages
   const commonLoader = async () => {
     const [
@@ -103,11 +58,11 @@ function App() {
       textCorpusStats,
       reportedSentencesStats,
     ] = await Promise.all([
-      loadCvLanguages(),
-      loadConfig(),
-      loadMatrix(),
-      loadTextCorpusStats(),
-      loadReportedSentencesStats(),
+      genericLoader(`${CV_DATA_URL}/$cv_languages.json`),
+      genericLoader(`${ANALYZER_DATA_URL}/$config.json`),
+      genericLoader(`${ANALYZER_DATA_URL}/$support_matrix.json`),
+      genericLoader(`${ANALYZER_DATA_URL}/$text_corpus_stats.json`),
+      genericLoader(`${ANALYZER_DATA_URL}/$reported.json`),
     ]);
     const loaderData: ILoaderData = {
       cvLanguages: cvLanguages,
