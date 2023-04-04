a30516450150231e1121264dd844b6be07a36f2a
Bülent Özden <bulentozden2007@gmail.com>
2023-04-04 05:27:52 +0300

    Remove many to local

--- web/src/stores/store.ts
+++ web/src/stores/store.ts
@@ -2,12 +2,7 @@
 import { create, StoreApi, UseBoundStore } from "zustand";
 
 // App
-import {
-  DATASET_INFO_ROW_TYPE,
-  DATASET_INFO_VIEW_TYPE,
-  TEXT_CORPUS_STATS_ROW_TYPE,
-  REPORTED_STATS_ROW_TYPE,
-} from "../helpers/tableHelper";
+import { DATASET_INFO_ROW_TYPE } from "../helpers/tableHelper";
 
 import { LanguageCodesType, DEFAULT_UI_LOCALE } from "../helpers/localeHelper";
 
@@ -24,16 +19,6 @@ export type StoreType = {
   // Big Data which have calculated fields
   //
 
-  // Text Corpus
-  textCorpusStats: TEXT_CORPUS_STATS_ROW_TYPE[] | undefined;
-  setTextCorpusStats: (data: TEXT_CORPUS_STATS_ROW_TYPE[] | undefined) => void;
-
-  // Reported Sentences
-  reportedSentencesStats: REPORTED_STATS_ROW_TYPE[] | undefined;
-  setReportedSentencesStats: (
-    data: REPORTED_STATS_ROW_TYPE[] | undefined,
-  ) => void;
-
   // Dataset Info for selected dataset(lc, ver)
   datasetInfo: DATASET_INFO_ROW_TYPE[] | undefined;
   setDatasetInfo: (data: DATASET_INFO_ROW_TYPE[] | undefined) => void;
@@ -46,14 +31,6 @@ export type StoreType = {
   // TODO : Expand to multiple datasets for comparison
   selectedDataset: string;
   setSelectedDataset: (dsver: string) => void;
-
-  // Text Corpus record for selected dataset
-  textCorpusRec: TEXT_CORPUS_STATS_ROW_TYPE | undefined;
-  setTextCorpusRec: (data: TEXT_CORPUS_STATS_ROW_TYPE | undefined) => void;
-
-  // Text Corpus record for selected dataset
-  reportedSentencesRec: REPORTED_STATS_ROW_TYPE | undefined;
-  setReportedSentencesRec: (data: REPORTED_STATS_ROW_TYPE | undefined) => void;
 
   // selected version filter
   versionFilter: string[];
@@ -79,16 +56,6 @@ const useStore: UseBoundStore<StoreApi<StoreType>> = create<StoreType>(
     // Big Data which have calculated fields
     //
 
-    // Text Corpus
-    textCorpusStats: undefined,
-    setTextCorpusStats: (data) =>
-      set((state) => ({ ...state, textCorpusStats: data })),
-
-    // Reported Sentences
-    reportedSentencesStats: undefined,
-    setReportedSentencesStats: (data) =>
-      set((state) => ({ ...state, reportedSentencesStats: data })),
-
     // Split Stats
     datasetInfo: undefined,
     setDatasetInfo: (data) => set((state) => ({ ...state, datasetInfo: data })),
@@ -102,16 +69,6 @@ const useStore: UseBoundStore<StoreApi<StoreType>> = create<StoreType>(
     selectedDataset: "",
     setSelectedDataset: (dsver) =>
       set((state) => ({ ...state, selectedDataset: dsver })),
-
-    // Text Corpus record for selected dataset
-    textCorpusRec: undefined,
-    setTextCorpusRec: (data) =>
-      set((state) => ({ ...state, textCorpusRec: data })),
-
-    // Text Corpus record for selected dataset
-    reportedSentencesRec: undefined,
-    setReportedSentencesRec: (data) =>
-      set((state) => ({ ...state, reportedSentencesRec: data })),
 
     // selected version filter
     versionFilter: [],
