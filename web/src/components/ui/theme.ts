6d145ad31d657a33c305443808c1b026530f482a
Bülent Özden <bulentozden2007@gmail.com>
2024-11-02 07:27:21 +0300

    Add primary color alternative for tables in expanded areas

--- web/src/components/ui/theme.ts
+++ web/src/components/ui/theme.ts
@@ -1,7 +1,8 @@
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
-export const PRIMARY_COLOR = "#b71c1c";;
-export const SECONDARY_COLOR = "#1cb7b7";;
+export const PRIMARY_COLOR = "#b71c1c";
+export const PRIMARY_DARK = "#8D1515";
+export const SECONDARY_COLOR = "#1cb7b7";
 
 const theme = createTheme({
   palette: {
